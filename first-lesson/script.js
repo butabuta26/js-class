17f6c9b0d0413854f42beb942ebe449f7aa35635
Anastasia Tsiklauri <147313565+butabuta26@users.noreply.github.com>
2026-01-28 16:55:57 +0400

    Add files via upload

---
+++ first-lesson/script.js
@@ -0,0 +1,44 @@
+// ! script.js, index.js, app.js
+
+// ! primitive data types
+
+// ! number
+// ! string
+// ! boolean
+// ! bigInt
+// ! null
+// ! undefined
+// ! symbol
+
+// console.log(8282736);
+// console.log("Mamaaaa, ooooh");
+
+// console.log('i am', 15, 'hehe');
+
+// var name = 'ana'
+
+// console.log(name);
+
+let first_name = 'ana'
+let firstName = 'gio'
+let age = 77
+
+console.log(first_name + ' ' + 'is' + ' ' + age + ' years old');
+
+console.log(`${firstName} is ${age} years old`);
+
+const firstNumber = 30
+const secondNumber = 44
+const answer = firstNumber + secondNumber
+
+console.log(answer);
+
+console.log();
+
+const myName = 'ana'
+const myLastName = 'tsiko'
+const myAge = 16
+const address = 'varketili'
+const dateOfBirth = 2009
+
+console.log(`My name is ${myName} ${myLastName} and I'm ${myAge} years old, born in ${dateOfBirth}. I live in ${address}.`);
